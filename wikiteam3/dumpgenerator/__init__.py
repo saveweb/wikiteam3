9375733b9c5aab77b8fe22085751a6d6154f82fd
yzqzss <yzqzss@yandex.com>
2024-03-05 11:08:57 +0800

    remove deprecated `dumpgenerator` script

--- wikiteam3/dumpgenerator/__init__.py
+++ wikiteam3/dumpgenerator/__init__.py
@@ -5,9 +5,3 @@ DUMPER_ROOT_PATH = Path(__file__).parent
 
 def main():
     DumpGenerator()
-
-
-def main_deprecated(): # poetry scripts dumpgenerator = "wikiteam3.dumpgenerator:main_deprecated"
-    import warnings
-    warnings.warn("dumpgenerator is deprecated, use wikiteam3dumpgenerator instead", DeprecationWarning)
-    main()
